c97de7d3abca99f1ad5d6fda08cc7b0360dc7c22
tony4win <149738877+tony4win@users.noreply.github.com>
2024-03-14 03:56:30 +0000

    Adding Registration Form

--- Warhammer/resources/views/home.blade.php
+++ Warhammer/resources/views/home.blade.php
@@ -4,14 +4,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="/resources/css/app.cs">
 </head>
 <body>
     <head>
         <h1>Home</h1>
     </head>
-    <body>
-        
-    </body>
+    <main>
+        <div>
+            <h3>Register</h3>
+            <form action="/register" method="POST">
+                @csrf
+                <input type="text" placeholder="username">
+                <input type="text" placeholder="email">
+                <input type="password" placeholder="password">
+                <button>Register</button>
+            </form>
+        </div>
+    </main>
     <footer>
 
     </footer>
