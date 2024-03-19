c568f15239a9cc60d02463b87127a9a75c353797
tony4win <149738877+tony4win@users.noreply.github.com>
2024-03-19 05:17:56 +0000

    Correcting @auth @else @enauth

--- Warhammer/resources/views/home.blade.php
+++ Warhammer/resources/views/home.blade.php
@@ -46,30 +46,30 @@
                 </div>
                 @endforeach
             </div>
+
         @else
-            
+        
+            <div>
+                <h3>Register</h3>
+                <form action="/register" method="POST">
+                    @csrf
+                    <input type="text" placeholder="Enter Username" name="name">
+                    <input type="text" placeholder="Enter Email" name="email">
+                    <input type="password" placeholder="Enter Password" name="password">
+                    <button>Register</button>
+                </form>
+            </div>
+
+            <div>
+                <h3>Login</h3>
+                <form action="/login" method="POST">
+                    @csrf
+                    <input type="text" placeholder="Enter Username" name="login_name">
+                    <input type="password" placeholder="Enter Password" name="login_password">
+                    <button>Login</button>
+                </form>
+            </div>
         @endauth
-
-        <div>
-            <h3>Register</h3>
-            <form action="/register" method="POST">
-                @csrf
-                <input type="text" placeholder="Enter Username" name="name">
-                <input type="text" placeholder="Enter Email" name="email">
-                <input type="password" placeholder="Enter Password" name="password">
-                <button>Register</button>
-            </form>
-        </div>
-
-        <div>
-            <h3>Login</h3>
-            <form action="/login" method="POST">
-                @csrf
-                <input type="text" placeholder="Enter Username" name="login_name">
-                <input type="password" placeholder="Enter Password" name="login_password">
-                <button>Login</button>
-            </form>
-        </div>
 
     </main>
     <footer>
